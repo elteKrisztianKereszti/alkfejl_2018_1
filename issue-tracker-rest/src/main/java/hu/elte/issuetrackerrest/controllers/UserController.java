92dfcaee448d3940a4be533c7d2cee6b9e00536f
Kereszti Krisztián <kereszti.krisztian@marktsoft.hu>
2018-10-30 18:46:35 +0100

    User Entity/Repo/Controller

---
+++ issue-tracker-rest/src/main/java/hu/elte/issuetrackerrest/controllers/UserController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.elte.issuetrackerrest.controllers;
+
+import hu.elte.issuetrackerrest.entities.User;
+import hu.elte.issuetrackerrest.repositories.UserRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author KeresztiKrisztián
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+    
+    @Autowired
+    private UserRepository userRepository;
+    
+    @PostMapping("register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        Optional<User> oUser = userRepository.findByUsername(user.getUsername());
+        if (oUser.isPresent()) {
+            return ResponseEntity.badRequest().build();
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setEnabled(true);
+        user.setRole(User.Role.ROLE_USER);
+        return ResponseEntity.ok(userRepository.save(user));
+    }
+    
+    @PostMapping("login")
+    public ResponseEntity login(@RequestBody User user) {
+        return ResponseEntity.ok().build();
+    }
+}
